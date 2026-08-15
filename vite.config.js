2bc7236ea13aa4af6c0bcfcb82402ee5bec7318b
Gopuabhayan <gopuabhayan5@gmail.com>
2026-08-15 14:35:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-apod-site/',
+  base: process.env.VERCEL ? '/' : '/my-apod-site/',
   plugins: [react()],
 })
