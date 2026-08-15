db735989b57c9fc8a40b907d95cc9cc52b9230ee
Gopuabhayan <gopuabhayan5@gmail.com>
2026-08-15 12:57:11 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-export default defineConfig(() => {
-  return {
-    define: {
-      __APP_ENV__: process.env.VITE_API_KEYZ,
-    },
-  };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
